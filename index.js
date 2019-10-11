946e448771990afb12c3591f7513464bec9982a6
Justin Kopepasah <justin@kopepasah.com>
2019-10-11 18:46:39 -0500

    Dropped the async, but not the await.

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const exec = require( '@actions/exec' );
 try {
 	const PANTHEON_MACHINE_TOKEN = core.getInput( 'pantheon-machine-token' );
 
-	await exec.exec( 'curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar' );
-	await exec.exec( 'sudo php installer.phar install' ); // Sudo is required in order to install bin/terminus.
-	await exec.exec( 'terminus', [ 'auth:login', `--machine-token=${ PANTHEON_MACHINE_TOKEN }` ] );
+	exec.exec( 'curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar' );
+	exec.exec( 'sudo php installer.phar install' ); // Sudo is required in order to install bin/terminus.
+	exec.exec( 'terminus', [ 'auth:login', `--machine-token=${ PANTHEON_MACHINE_TOKEN }` ] );
 } catch ( error ) {
 	core.setFailed( error.message );
 }
