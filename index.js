3c288bac4a78b009ae21d74c0d1545f05b95d7f4
German Licon <german.licon@achieveinternet.com>
2020-09-03 16:25:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const exec = require( '@actions/exec' );
 async function run() {
 	try {
 		const PANTHEON_MACHINE_TOKEN = core.getInput( 'pantheon-machine-token' );
-
+		await exec.exec( 'sudo mkdir -p ~/terminus' );
 		await exec.exec( 'curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar' );
 		await exec.exec( 'sudo php installer.phar install --install-dir=~/terminus' ); // Sudo is required in order to install bin/terminus.
 		await exec.exec( 'terminus', [ 'auth:login', `--machine-token=${ PANTHEON_MACHINE_TOKEN }` ] );
